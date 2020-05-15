42254e91144f169eceb526f391c5999c43934641
Rick Yang <rick68@gmail.com>
2020-05-15 21:18:07 +0800

    add scripts run-postgres.sh and stop-postgres.sh

---
+++ nix/scripts/run-postgres.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env nix-shell
+#! nix-shell -i /bin/sh -p gnumake docker diesel-cli
+
+TOP_DIR="$(dirname $(readlink -f $0))"
+
+while [ ! -f $TOP_DIR/Makefile ]; do
+    TOP_DIR=$(dirname $TOP_DIR)
+done
+
+make -C $TOP_DIR CONTAINER_RUNTIME=docker run-postgres
