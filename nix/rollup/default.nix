96e9744432f9839c18085ca2807cefa236b2923f
Rick Yang <rick68@gmail.com>
2020-09-30 07:14:43 +0800

    nix/rollup: improve the default.nix

--- nix/rollup/default.nix
+++ nix/rollup/default.nix
@@ -1,20 +1,23 @@
-{ nixpkgs ? <nixpkgs>
-, pkgs ? (import nixpkgs {})
+{ pkgs ? import <nixpkgs> {}
 , stdenv ? pkgs.stdenv
+, runCommandLocal ? pkgs.runCommandLocal
 }:
 
 let
+  nodeEnv = "production";
   nodePackages = import ./nodepkgs.nix {
     inherit pkgs;
     inherit (stdenv) system;
   };
 
-  version = builtins.readFile (
-    pkgs.runCommandLocal "version"
-      { PACKAGE_JSON = builtins.readFile ./pkgs.json; } ''
-        echo "$PACKAGE_JSON" | ${pkgs.jq}/bin/jq '.[] | .["rollup"]' | tr -d '"\n' > $out
-      ''
-  );
+  name = builtins.readFile (runCommandLocal "name"
+    { PKGS_JSON = builtins.readFile ./pkgs.json; } ''
+      echo "$PKGS_JSON" | ${pkgs.jq}/bin/jq '.[] | keys | .[0]' | tr -d '"\n' > $out
+    '');
 
-in
-nodePackages."rollup-${version}"
+  version = builtins.readFile (runCommandLocal "version"
+    { PKGS_JSON = builtins.readFile ./pkgs.json; } ''
+      echo "$PKGS_JSON" | ${pkgs.jq}/bin/jq '.[] | .[]' | tr -d '"\n' > $out
+    '');
+
+in nodePackages."${name}-${version}"
